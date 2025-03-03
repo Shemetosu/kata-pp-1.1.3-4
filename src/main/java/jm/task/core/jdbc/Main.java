8077a59ec63f87f9b96eef64a0407db9e6742061
Vladimir Shemetenko <ov.shtk@gmail.com>
2025-03-03 21:03:06 +0300

    Проблемы с мерж Маин

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -13,20 +13,16 @@ public class Main {
         UserService userService = new UserServiceImpl();
 
         userService.createUsersTable();
-        Util.closeConnection();
 
         userService.saveUser("Alex", "Smith1", (byte) 25);
         userService.saveUser("John", "Smith2", (byte) 46);
         userService.saveUser("Fedor", "Smith3", (byte) 37);
         userService.saveUser("Igor", "Smith4", (byte) 18);
-        Util.closeConnection();
 
         List<User> usersList = userService.getAllUsers();
         usersList.forEach(System.out::println);
-        Util.closeConnection();
 
         userService.cleanUsersTable();
-        Util.closeConnection();
 
         userService.dropUsersTable();
         Util.closeConnection();
